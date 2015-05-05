2708b9ced1d1d0728f8f871c4fb7631b96d56c78
Andrew <flyingshadow64@gmail.com>
2015-05-04 20:35:34 -0400

    commit 2

--- homework3.php
+++ homework3.php
@@ -2,5 +2,29 @@
 
 
 
+error_reporting(-1);
+
+ini_set('display_errors', 'On');
+
+$hostname = 'mysql:host=localhost; dbname=employees';
+$user = 'root';
+$pass = 'alexhong';
+$connect = new PDO ($hostname,$user,$pass);
+
+
+$sql = 'Select * from employees left join salaries on employees.emp_no=salaries.emp_no order by salary DESC limit 5';
+
+foreach($connect->query($sql) as $row) {
+
+print_r($row);
+
+
+
+}
+
+print_r($connect);
+
+
+
 
 ?>
