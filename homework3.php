1bf6505a142b4ff3675c047c97740a79e66f157f
Andrew <flyingshadow64@gmail.com>
2015-05-05 17:51:55 -0400

    homework3 commit 3

--- homework3.php
+++ homework3.php
@@ -1,30 +1,54 @@
-<?php
-
-
-
-error_reporting(-1);
-
-ini_set('display_errors', 'On');
-
-$hostname = 'mysql:host=localhost; dbname=employees';
+<!DOCTYPE html>
+<html lang = "en-US">
+ <head>
+ <meta charset = "UTF-8">
+ <title>contact.php</title>
+ <style type = "text/css">
+  table, th, td {border: 1px solid black};
+ </style>
+ </head>
+ <body>
+ <p>
+ <?php
+$hostname ='mysql:host=localhost; dbname=employees';
 $user = 'root';
 $pass = 'alexhong';
-$connect = new PDO ($hostname,$user,$pass);
+try {
+	$connect= new PDO($hostname, $user, $pass);
+	$connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$sql = "SELECT employees.emp_no, employees.first_name, employees.last_name, salaries.salary from employees left join salaries on employees.emp_no=salaries.emp_no order by salary DESC limit 5";
+ 
+  
+	print "<table> \n";
+	$result = $connect->query($sql);
 
+	$row = $result->fetch(PDO::FETCH_ASSOC);
+		print " <tr> \n";
+			foreach ($row as $columnName => $value)
+		{
+			print " <th>$columnName</th> \n";
+		} 
+	print " </tr> \n";
 
-$sql = 'Select * from employees left join salaries on employees.emp_no=salaries.emp_no order by salary DESC limit 5';
-
-foreach($connect->query($sql) as $row) {
-
-print_r($row);
-
-
-
+	$data = $connect->query($sql);
+	$data->setFetchMode(PDO::FETCH_ASSOC);
+	foreach($data as $row)
+		{
+			print " <tr> \n";
+			foreach ($row as $name=>$value)
+				{
+					print " <td>$value</td> \n";
+				} 
+			print " </tr> \n";
+		}
+	print "</table> \n";
 }
+catch(PDOException $e) 
+{
+   echo 'ERROR: ' . $e->getMessage();
+}
+ ?>
+ </p>
+ </body>
+</html>
 
-print_r($connect);
-
-
-
-
-?>
